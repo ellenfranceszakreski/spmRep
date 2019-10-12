39deb106d57e39c20b8020dc0143a91c807fbe5d
ellenfranceszakreski <efzakreski@mac.com>
2019-10-12 19:18:59 -0400

    Create Level2v1_run.m

---
+++ Level2v1_run.m
@@ -0,0 +1,20 @@
+% Level2vx
+Level2vx = 'Level2v1';
+AnalysisDir='/data/scratch/zakell/fmri_oct2019';
+
+addpath(fullfile(AnalysisDir,'Scripts'));
+addpath(genpath(fullfile(spm('dir'),'config')))
+
+con_000xs={'con_0001','con_0002'};
+nrun = numel(con_000xs);
+
+jobs = {fullfile(AnalysisDir, 'Scripts', [Level2vx,'_matlabbatch.m'])};
+jobs = repmat(jobs, 1, nrun);
+inputs = cell(13, nrun);
+for crun=1:nrun
+  inputs(1:13,crun) = Level2v1_con_000x_Inputs(AnalysisDir, con_000xs{crun}); 
+end
+
+spm('defaults','FMRI');
+spm_jobman('run', jobs, inputs{:});
+% done
